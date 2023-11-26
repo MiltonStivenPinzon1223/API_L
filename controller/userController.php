95239753b10d132e86d380385939672fa7ae4f4c
LinaCruzM <miltonstivenpinzon@gmail.com>
2023-11-26 02:36:12 -0500

    pre estructuracion

--- controller/userController.php
+++ controller/userController.php
@@ -65,15 +65,15 @@ class UserController{
         $trimmed_data="";
         //var_dump($this->_data);
         $pattern = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/';
-        if (preg_match($pattern, $this->_data['use_pss'])) {
+        if (preg_match($pattern, $this->_data['use_pass'])) {
             if(($this->_data !="") || (!empty($this->_data))){
                 $trimmed_data = array_map('trim', $this->_data);
-                $trimmed_data['use_pss'] = md5($trimmed_data['use_pss']);
+                $trimmed_data['use_pass'] = md5($trimmed_data['use_pass']);
                 //salting
-                $identifier = str_replace("$", "y78", crypt($trimmed_data['use_mail'], 'ser3478'));
-                $key = str_replace("$", "ERT", crypt($trimmed_data['use_pss'], '$uniempresarial2024'));
+                $identifier = str_replace("$", "y78", crypt($trimmed_data['use_email'], 'ser3478'));
+                $key = str_replace("$", "ERT", crypt($trimmed_data['use_pass'], '$uniempresarial2024'));
                 $trimmed_data['us_identifier'] = $identifier;
-                $trimmed_data['us_key'] = $key;
+                $trimmed_data['use_key'] = $key;
                 return $trimmed_data;
             }
         } else {
